1cbe4d81480ac440382f99afaac06fe78f928b40
Melker Larsson <melker.programmering@gmail.com>
2024-09-17 22:07:22 +0200

    primes!

--- createprimes.py
+++ createprimes.py
@@ -8,15 +8,15 @@ def is_prime(i, primes):
                 return False
     return True
 
-
-for i in range(2, 10**6):
-    if is_prime(i, primes):
-        primes.append(i)
-
-    if i % 1000 == 0:
-        print(i)
-
 with open("primes.txt", 'w') as file:
-    for prime in primes:
-        file.write(f"{prime}\n")
+
+    for i in range(2, 10**9):
+        if is_prime(i, primes):
+            primes.append(i)
+            file.write(f"{i}\n")
+
+
+        if i % 1000 == 0:
+            print(i)
+
 
